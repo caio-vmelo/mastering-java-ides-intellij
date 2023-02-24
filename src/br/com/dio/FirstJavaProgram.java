a8a0d479f197d96eedb699647c6192aacc164689
caio-vmelo <viniciuscaioml@gmail.com>
2023-02-24 12:48:41 -0300

    creating class book

--- src/br/com/dio/FirstJavaProgram.java
+++ src/br/com/dio/FirstJavaProgram.java
@@ -5,11 +5,48 @@ import br.com.dio.model.cat;
 public class FirstJavaProgram {
     public static void main(String[] args) {
         cat gato = new cat();
-
         System.out.println(gato);
+
+        Book book1 = new Book("O problema dos 3 porcos", 300);
+        System.out.println(book1);
+
 
         /*int a = 10;
         int b = 10;
         System.out.println("Hello World! " + (a + b));*/
+    }
+}
+
+class Book {
+    private String name;
+    private Integer numPages;
+
+    public Book(String name, Integer numPages) {
+        this.name = name;
+        this.numPages = numPages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(Integer numPages) {
+        this.numPages = numPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", numPages=" + numPages +
+                '}';
     }
 }
