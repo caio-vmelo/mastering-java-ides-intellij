f32140a684f35e9535f78fb1b9cf364054fbc477
caio-vmelo <viniciuscaioml@gmail.com>
2023-02-24 12:40:06 -0300

    first java commit

---
+++ src/br/com/dio/FirstJavaProgram.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.cat;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        cat gato = new cat();
+
+        System.out.println(gato);
+
+        /*int a = 10;
+        int b = 10;
+        System.out.println("Hello World! " + (a + b));*/
+    }
+}
